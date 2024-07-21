fde3617d49f597d96918eaf0877e265c54c96a05
Gareth Beukes <garethb@learningsandbox.com>
2024-07-21 20:41:12 +0200

    Update UX for the search component

--- angular-github-api-app/src/app/components/search/search.component.ts
+++ angular-github-api-app/src/app/components/search/search.component.ts
@@ -5,8 +5,10 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
 import { MatButtonModule } from '@angular/material/button';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { RouterModule } from '@angular/router';
 import { GithubService } from '../../services/github.service';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-search',
@@ -20,18 +22,37 @@ import { GithubService } from '../../services/github.service';
     MatInputModule,
     MatListModule,
     MatButtonModule,
+    MatProgressSpinnerModule,
     RouterModule
+  ],
+  animations: [
+    trigger('fadeInOut', [
+      state('void', style({ opacity: 0, height: '0px' })),
+      state('*', style({ opacity: 1, height: '*' })),
+      transition(':enter, :leave', [animate('300ms ease-in-out')])
+    ])
   ]
 })
 export class SearchComponent {
   repositories: any[] = [];
   searchQuery: string = '';
+  loading: boolean = false;
+  inputFocused: boolean = false;
+  searchPerformed: boolean = false;
+  currentSearchQuery: string = '';
 
   constructor(private githubService: GithubService) {}
 
   searchRepositories() {
+    if (!this.searchQuery || this.searchQuery === this.currentSearchQuery) {
+      return;
+    }
+    this.loading = true;
+    this.currentSearchQuery = this.searchQuery;
     this.githubService.searchRepositories(this.searchQuery).subscribe((data: any) => {
       this.repositories = data.items;
+      this.loading = false;
+      this.searchPerformed = true;
     });
   }
 }
--- angular-github-api-app/src/main.ts
+++ angular-github-api-app/src/main.ts
@@ -1,6 +1,11 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
+import { provideAnimations } from '@angular/platform-browser/animations';
 import { AppComponent } from './app/app.component';
+import { appConfig } from './app/app.config';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideAnimations(),
+    ...appConfig.providers
+  ]
+});
