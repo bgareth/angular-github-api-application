5dce36ba025cb63383d658a16ba2d947899dc73c
Gareth Beukes <garethb@learningsandbox.com>
2024-07-22 02:06:36 +0200

    styling updates

--- angular-github-api-app/src/app/components/issues-list/issues-list.component.ts
+++ angular-github-api-app/src/app/components/issues-list/issues-list.component.ts
@@ -6,9 +6,12 @@ import { MatSelectModule } from '@angular/material/select';
 import { MatOptionModule } from '@angular/material/core';
 import { MatListModule } from '@angular/material/list';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { Location } from '@angular/common';
 import { GithubService } from '../../services/github.service';
 import { NavigationService } from '../../services/navigation.service';
+import { trigger, state, style, transition, animate, query, stagger } from '@angular/animations';
 
 @Component({
   selector: 'app-issues-list',
@@ -22,13 +25,33 @@ import { NavigationService } from '../../services/navigation.service';
     MatOptionModule,
     MatListModule,
     MatButtonModule,
+    MatIconModule,
+    MatProgressSpinnerModule, // Add MatProgressSpinnerModule here
     RouterModule
+  ],
+  animations: [
+    trigger('fadeInOut', [
+      state('void', style({ opacity: 0, height: '0px' })),
+      state('*', style({ opacity: 1, height: '*' })),
+      transition(':enter, :leave', [animate('300ms ease-in-out')])
+    ]),
+    trigger('listAnimation', [
+      transition('* => *', [
+        query(':enter', [
+          style({ opacity: 0, transform: 'translateX(-50%)' }),
+          stagger(100, [
+            animate('150ms ease-out', style({ opacity: 1, transform: 'translateX(0)' }))
+          ])
+        ], { optional: true })
+      ])
+    ])
   ]
 })
 export class IssuesListComponent implements OnInit {
   issues: any[] = [];
   filteredIssues: any[] = [];
   filterState: string = 'all';
+  loading: boolean = true;
 
   constructor(
     private route: ActivatedRoute,
@@ -46,9 +69,11 @@ export class IssuesListComponent implements OnInit {
       this.githubService.getRepositoryIssues(owner, repo).subscribe((data: any) => {
         this.issues = data;
         this.filterIssues();
+        this.loading = false;
       });
     } else {
       console.error('Owner or repository name is null');
+      this.loading = false;
     }
   }
 
